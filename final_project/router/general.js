daec8755c388e0d911fb2d215ae02594653094c5
lijinmu <lijin180@gmail.com>
2025-03-25 14:37:35 -0400

    authuser

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,14 +1,32 @@
 const express = require('express');
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
+let authenticatedUser = require("./auth_users.js").authenticatedUser;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
-
+const jwt = require('jsonwebtoken');
+const session = require('express-session');
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+    const password = req.body.password;
+    // Check if both username and password are provided
+    if (username && password) {
+        // Check if the user does not already exist
+        if (!isValid(username)) {
+            // Add the new user to the users array
+            users.push({"username": username, "password": password});
+            return res.status(200).json({message: "User successfully registered. Now you can login"});
+        } else {
+            return res.status(404).json({message: "User already exists!"});
+        }
+    }
+    // Return error if username or password is missing
+    return res.status(404).json({message: "Unable to register user."});
 });
+
+
+
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
